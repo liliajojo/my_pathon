40d7b33966a85a233f9ad993c0ee526da90d7541
liliajojo <uanickaalilia@gmail.com>
2021-01-26 20:11:17 +0900

    modifile

--- syntax. 1/1.variables.py
+++ syntax. 1/1.variables.py
@@ -1,1 +1,86 @@
-print(input("Введите что-то: "))
+# вызов функции печати в терминал (print)
+
+#print("Привет,'мир'")
+
+#вызов функции ввода из терминала (input)
+
+#print(input("Введите что-то: "))
+
+# *** переменные ***
+pot = 30
+pyt = 20
+
+k = pot + pyt
+
+#print(k)
+
+# *** типы данных ***
+# простые типы 
+
+int_num = 1000 #целое число
+
+floatNum = 3.14 # числа с плавающей точкой 
+
+my_str = "Привет мир"
+
+boolean = True
+
+#print(int_num * floatNum)
+
+#print(int_num * 2)
+
+result_1 = my_str * 4 # умножение строк 
+
+result_2 = my_str + ' ' + "wow"
+
+#print(result_1, result_2)
+
+# *** способ форматирования строк f-srting ***
+
+name = "Лиля"
+age =  18 
+
+s = f"Hello, how are u {name}? Age: {age}."
+
+#print(s)
+
+
+# Арифметические операции 
+
+x = 2
+y = 2.4
+
+res_l = x + y 
+
+res_2 = x * y
+
+res_3 = x / y # обычное деление
+
+res_4 = x // y #целочисленное деление
+
+res_5 = x % y # деление по остатку
+
+res_6 = x ** y # возведение в степень 
+
+import math 
+
+res_7 = math.sqrt(1024) # излечение кв.корня
+
+#print(res_7)
+
+# Логические операции 
+
+x = 4
+y = 8
+
+res_l = x != y 
+
+res_2 = x == y
+
+res_3 = x < y
+
+res_4 = x > y
+
+res_5 = x >= y
+
+print(res_5)
